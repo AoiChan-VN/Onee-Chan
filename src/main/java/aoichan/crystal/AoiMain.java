b526ad1ca0ff5c4070db67acac643976c1ef03f1
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:47:23 +0700

    Update AoiMain.java

--- src/main/java/aoichan/crystal/AoiMain.java
+++ src/main/java/aoichan/crystal/AoiMain.java
@@ -1,10 +1,11 @@
 package aoichan.crystal;
 
 import aoichan.crystal.api.GemsAPI;
-import aoichan.crystal.core.*;
-import aoichan.crystal.gui.*;
-import aoichan.crystal.storage.*;
 import aoichan.crystal.commands.GemsCommand;
+import aoichan.crystal.core.*;
+import aoichan.crystal.gui.GUIListener;
+import aoichan.crystal.storage.*;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AoiMain extends JavaPlugin {
@@ -17,7 +18,9 @@ public final class AoiMain extends JavaPlugin {
     private SocketManager socketManager;
     private GemsAPI api;
 
-    public static AoiMain get() { return instance; }
+    public static AoiMain get() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
@@ -26,38 +29,29 @@ public final class AoiMain extends JavaPlugin {
         saveDefaultConfig();
         saveResource("gems.yml", false);
 
-        if (getConfig().getBoolean("banner.enabled"))
+        if (getConfig().getBoolean("banner.enabled", true)) {
             ConsoleBanner.show(this);
+        }
 
-        setupDatabase();
-        setupManagers();
-        registerSystems();
-
-        getLogger().info("GemsUltimate 2.0 loaded safely.");
-    }
-
-    private void setupDatabase() {
         databasePool = new DatabasePool(this);
-        storage = databasePool.createProvider();
+        storage = new SQLiteStorage(databasePool);
         storage.initTables();
-    }
 
-    private void setupManagers() {
         gemsManager = new GemsManager(this);
         socketManager = new SocketManager(this);
         api = new GemsAPI(gemsManager, socketManager);
-    }
 
-    private void registerSystems() {
         getCommand("gems").setExecutor(new GemsCommand());
+
         getServer().getPluginManager().registerEvents(
                 new AntiDupeManager(socketManager), this);
+
         getServer().getPluginManager().registerEvents(
                 new GUIListener(), this);
     }
 
     public GemsAPI getAPI() { return api; }
-    public StorageProvider getStorage() { return storage; }
+    public GemsManager getGemsManager() { return gemsManager; }
 
     @Override
     public void onDisable() {
