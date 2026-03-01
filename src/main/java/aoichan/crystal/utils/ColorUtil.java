f94bb55cafe5759131e78fb1a47faf63ea01b7d9
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-02 01:13:12 +0700

    Update ColorUtil.java

--- src/main/java/aoichan/crystal/utils/ColorUtil.java
+++ src/main/java/aoichan/crystal/utils/ColorUtil.java
@@ -1,1 +1,19 @@
+package aoichan.crystal.utils;
 
+import org.bukkit.ChatColor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ColorUtil {
+
+    public static String colorize(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    public static List<String> colorizeList(List<String> list) {
+        return list.stream()
+                .map(ColorUtil::colorize)
+                .collect(Collectors.toList());
+    }
+}
