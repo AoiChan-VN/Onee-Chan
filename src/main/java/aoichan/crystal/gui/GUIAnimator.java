59c2a1d94ad09ba6aa8e7bc8d471f0bfb8ba3039
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:37:46 +0700

    Update GUIAnimator.java

--- src/main/java/aoichan/crystal/gui/GUIAnimator.java
+++ src/main/java/aoichan/crystal/gui/GUIAnimator.java
@@ -1,1 +1,24 @@
+package aoichan.crystal.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
+public class GUIAnimator {
+
+    public static void animate(Inventory inv) {
+
+        Bukkit.getScheduler().runTaskTimer(
+            Bukkit.getPluginManager().getPlugin("GemsUltimate"),
+            new Runnable() {
+
+                int slot = 0;
+
+                @Override
+                public void run() {
+                    if (slot >= inv.getSize()) slot = 0;
+                    inv.setItem(slot, null);
+                    slot++;
+                }
+            }, 0L, 2L);
+    }
+}
