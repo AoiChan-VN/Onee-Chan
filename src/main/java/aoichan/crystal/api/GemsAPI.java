f4b512c90da4bc28ea7ba888e330ecc060fea959
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:05:21 +0700

    Update GemsAPI.java

--- src/main/java/aoichan/crystal/api/GemsAPI.java
+++ src/main/java/aoichan/crystal/api/GemsAPI.java
@@ -1,1 +1,30 @@
+package aoichan.crystal.api;
 
+import aoichan.crystal.core.GemsManager;
+import aoichan.crystal.core.SocketManager;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Set;
+
+public class GemsAPI {
+
+    private final GemsManager gemsManager;
+    private final SocketManager socketManager;
+
+    public GemsAPI(GemsManager gm, SocketManager sm) {
+        this.gemsManager = gm;
+        this.socketManager = sm;
+    }
+
+    public Set<String> getAllGems() {
+        return gemsManager.getAllGems();
+    }
+
+    public ItemStack createGemItem(String id) {
+        return gemsManager.buildGem(id);
+    }
+
+    public void socket(ItemStack item, String gemId) {
+        socketManager.attachGem(item, gemId);
+    }
+}
