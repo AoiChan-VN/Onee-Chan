211b24a976d694d5b8ae3431e4c831c0cfe3d5e1
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 23:22:19 +0700

    Update GemsManager.java

--- src/main/java/aoichan/crystal/core/GemsManager.java
+++ src/main/java/aoichan/crystal/core/GemsManager.java
@@ -1,28 +1,46 @@
 package aoichan.crystal.core;
 
 import aoichan.crystal.AoiMain;
+import aoichan.crystal.utils.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
 import java.io.File;
 import java.util.*;
 
 public class GemsManager {
 
     private final Map<String, Map<String, Object>> gems = new HashMap<>();
+    private final AoiMain plugin;
 
     public GemsManager(AoiMain plugin) {
+        this.plugin = plugin;
+        reload();
+    }
+
+    public void reload() {
+        gems.clear();
         File file = new File(plugin.getDataFolder(), "gems.yml");
         YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
 
         for (String key : config.getConfigurationSection("gems").getKeys(false)) {
-            gems.put(key, config.getConfigurationSection("gems." + key).getValues(false));
+            gems.put(key,
+                    config.getConfigurationSection("gems." + key)
+                            .getValues(false));
         }
-    }
-
-    public Map<String, Object> getGem(String id) {
-        return gems.get(id);
     }
 
     public Set<String> getAllGems() {
         return gems.keySet();
+    }
+
+    public ItemStack buildGem(String id) {
+        Map<String, Object> data = gems.get(id);
+        if (data == null) return new ItemStack(Material.BARRIER);
+
+        return new ItemBuilder(Material.DIAMOND)
+                .setName((String) data.get("display-name"))
+                .build();
     }
 }
