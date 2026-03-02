deea24d1b0982b38a051d4159d752aa41feb8e8d
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-03 00:57:54 +0700

    Update ProtectionManager.java

--- src/main/java/aoichan/crystal/core/ProtectionManager.java
+++ src/main/java/aoichan/crystal/core/ProtectionManager.java
@@ -1,1 +1,35 @@
+package aoichan.crystal.core;
 
+import aoichan.crystal.AoiMain;
+import aoichan.crystal.utils.PDCUtil;
+import org.bukkit.inventory.ItemStack;
+
+public class ProtectionManager {
+
+    private final AoiMain plugin;
+
+    public ProtectionManager(AoiMain plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean isPermanentProtected(ItemStack item) {
+        return PDCUtil.has(item, "permanent_protect");
+    }
+
+    public boolean consumeProtection(ItemStack item) {
+
+        if (PDCUtil.has(item, "protect_once")) {
+            PDCUtil.remove(item, "protect_once");
+            return true;
+        }
+        return false;
+    }
+
+    public void applyPermanent(ItemStack item) {
+        PDCUtil.set(item, "permanent_protect", 1);
+    }
+
+    public void applyOnce(ItemStack item) {
+        PDCUtil.set(item, "protect_once", 1);
+    }
+}
