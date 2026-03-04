1bcf4b734cbea39bb8cbe64acfde3c2dc0d32c40
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-04 14:05:59 +0700

    Update StorageProvider.java

--- src/main/java/aoichan/crystal/storage/StorageProvider.java
+++ src/main/java/aoichan/crystal/storage/StorageProvider.java
@@ -1,6 +1,14 @@
-package aoichan.crystal.storage;
-
 public interface StorageProvider {
-    void initTables();
+
+    // code của: khởi tạo database
+    void init();
+
+    // code của: lưu dữ liệu
+    void saveData(java.util.UUID uuid, int gems);
+
+    // code của: load dữ liệu
+    int loadData(java.util.UUID uuid);
+
+    // code của: đóng kết nối
     void close();
 }
