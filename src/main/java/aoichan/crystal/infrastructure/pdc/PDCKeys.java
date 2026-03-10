1a3c9be572537e7036a8503614c7dbc9e6a04935
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-10 16:46:56 +0700

    Update PDCKeys.java

--- src/main/java/aoichan/crystal/infrastructure/pdc/PDCKeys.java
+++ src/main/java/aoichan/crystal/infrastructure/pdc/PDCKeys.java
@@ -13,4 +13,11 @@ public class PDCKeys {
     public static final NamespacedKey SOCKETS =
             new NamespacedKey(CrystalPlugin.get(), "crystal_sockets");
 
+    // [!] Code: Gem name id
+    public static final NamespacedKey GEM_ID =
+        new NamespacedKey(CrystalPlugin.get(), "gem_id");
+
+    // [!] Code: Gem level
+    public static final NamespacedKey GEM_LEVEL =
+        new NamespacedKey(CrystalPlugin.get(), "gem_level");
 } 
