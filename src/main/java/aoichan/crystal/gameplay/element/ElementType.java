eaccdcacdd4dc80b5bad71459e37c8dd7b2304b1
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-11 04:39:18 +0700

    Update ElementType.java

--- src/main/java/aoichan/crystal/gameplay/element/ElementType.java
+++ src/main/java/aoichan/crystal/gameplay/element/ElementType.java
@@ -1,6 +1,6 @@
 package aoichan.crystal.gameplay.element;
 
-// [!] Code: Element types
+// [!] Code: Element enum
 public enum ElementType {
 
     FIRE,
@@ -8,6 +8,19 @@ public enum ElementType {
     THUNDER,
     HOLY,
     VOID,
-    NONE
+    NONE;
 
-} 
+    public static ElementType fromString(String s) {
+
+        if (s == null) return NONE;
+
+        try {
+
+            return ElementType.valueOf(s.toUpperCase());
+
+        } catch (Exception e) {
+
+            return NONE;
+        }
+    }
+}
