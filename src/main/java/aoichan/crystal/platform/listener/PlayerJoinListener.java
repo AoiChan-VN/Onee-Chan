2bea2f8831060a4247d7b9ce0d69732d61c7a862
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-14 10:18:54 +0700

    Update PlayerJoinListener.java

--- src/main/java/aoichan/crystal/platform/listener/PlayerJoinListener.java
+++ src/main/java/aoichan/crystal/platform/listener/PlayerJoinListener.java
@@ -1,16 +1,1 @@
-package aoichan.crystal.platform.listener;
 
-import aoichan.crystal.gameplay.stat.StatUpdateService;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-
-public class PlayerJoinListener implements Listener {
-
-    @EventHandler
-    public void join(PlayerJoinEvent event) {
-
-        // [!] Code: Update stats
-        StatUpdateService.update(event.getPlayer());
-    }
-} 
