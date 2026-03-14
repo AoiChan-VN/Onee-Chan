8f7dbc7c14a46282acaff86bc478b363099645b9
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-14 19:01:02 +0700

    Update LogUtil.java

--- src/main/java/aoichan/crystal/infrastructure/util/LogUtil.java
+++ src/main/java/aoichan/crystal/infrastructure/util/LogUtil.java
@@ -1,18 +1,18 @@
 package aoichan.crystal.infrastructure.util;
 
-import aoichan.crystal.bootstrap.CrystalPlugin;
+import aoichan.crystal.bootstrap.AoiMain;
 
 public class LogUtil {
 
     public static void info(String message) {
 
-        CrystalPlugin.get().getLogger().info(message);
+        AoiMain.get().getLogger().info(message);
 
     }
 
     public static void warn(String message) {
 
-        CrystalPlugin.get().getLogger().warning(message);
+        AoiMain.get().getLogger().warning(message);
 
     }
 
