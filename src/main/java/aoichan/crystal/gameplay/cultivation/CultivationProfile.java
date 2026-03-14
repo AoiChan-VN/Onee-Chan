c1ba2d395c8512f55ad93b13fd17af4a8a271621
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-15 06:59:13 +0700

    Create CultivationProfile.java

---
+++ src/main/java/aoichan/crystal/gameplay/cultivation/CultivationProfile.java
@@ -0,0 +1,22 @@
+package aoichan.crystal.gameplay.cultivation;
+
+public class CultivationProfile {
+
+    private String realm;
+
+    public CultivationProfile(String realm) {
+
+        // 【!】Code: cảnh giới hiện tại
+        this.realm = realm;
+
+    }
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public void setRealm(String realm) {
+        this.realm = realm;
+    }
+
+} 
