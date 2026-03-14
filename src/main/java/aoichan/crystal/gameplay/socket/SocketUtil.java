f47b6be8eb2d0b311fa46c89fcc0b267d3d9adc4
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-14 23:21:56 +0700

    Create SocketUtil.java

---
+++ src/main/java/aoichan/crystal/gameplay/socket/SocketUtil.java
@@ -0,0 +1,12 @@
+package aoichan.crystal.gameplay.socket;
+
+import aoichan.crystal.bootstrap.CrystalPlugin;
+import org.bukkit.NamespacedKey;
+
+public class SocketUtil {
+
+    // 【!】Code: key lưu socket
+    public static final NamespacedKey SOCKET_KEY =
+            new NamespacedKey(CrystalPlugin.get(), "crystal_sockets");
+
+} 
