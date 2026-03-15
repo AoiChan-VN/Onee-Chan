0ea9f8f07e4911b76c5f1d6a79a2bd01e9b8153f
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-16 00:44:24 +0700

    Update AoiMain.java

--- src/main/aoi/aoichan/AoiMain.java
+++ src/main/aoi/aoichan/AoiMain.java
@@ -1,1 +1,53 @@
+package aoi.aoichan;
 
+import aoi.aoichan.command.EngineCommand;
+import aoi.aoichan.config.ConfigManager;
+import aoi.aoichan.core.EngineCore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AoiMain extends JavaPlugin {
+
+    // 【!】Code: Singleton instance để plugin khác truy cập Engine
+    private static AoiMain instance;
+
+    // 【!】Code: Core engine
+    private EngineCore engineCore;
+
+    public static AoiMain getInstance() {
+        return instance;
+    }
+
+    public EngineCore getEngineCore() {
+        return engineCore;
+    }
+
+    @Override
+    public void onEnable() {
+
+        // 【!】Code: khởi tạo instance
+        instance = this;
+
+        // 【!】Code: load config
+        ConfigManager.init(this);
+
+        // 【!】Code: khởi động engine
+        engineCore = new EngineCore(this);
+        engineCore.start();
+
+        // 【!】Code: register command
+        getCommand("aoiengine").setExecutor(new EngineCommand());
+
+        getLogger().info("AoiEngine Started.");
+    }
+
+    @Override
+    public void onDisable() {
+
+        // 【!】Code: shutdown engine
+        if(engineCore != null){
+            engineCore.shutdown();
+        }
+
+        getLogger().info("AoiEngine Shutdown.");
+    }
+}
