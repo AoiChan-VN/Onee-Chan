85b98714d2a2320fd1cfa5254ca095d07c54edeb
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-16 00:49:25 +0700

    Create EngineAPI.java

---
+++ src/main/aoi/aoichan/api/EngineAPI.java
@@ -0,0 +1,15 @@
+package aoi.aoichan.api;
+
+import aoi.aoichan.AoiMain;
+import aoi.aoichan.core.EngineCore;
+
+public class EngineAPI {
+
+    // 【!】Code: lấy engine core
+    public static EngineCore getEngine(){
+
+        return AoiMain.getInstance().getEngineCore();
+
+    }
+
+} 
