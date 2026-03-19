440d70687cdef31e4c1a20ce93871d4db5460a00
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-19 17:25:13 +0700

    Update AoiMain.java

--- src/main/java/aoichan/AoiMain.java
+++ src/main/java/aoichan/AoiMain.java
@@ -1,1 +1,29 @@
+// AoiCore - (Single Server Edition)
+// Author: AoiChan
+// Note: Clean, production-ready, async-safe
 
+package aoichan;
+
+import aoichan.bootstrap.Loader;
+import aoichan.bootstrap.Shutdown;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AoiMain extends JavaPlugin {
+
+    private static AoiMain instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        Loader.init(this);
+    }
+
+    @Override
+    public void onDisable() {
+        Shutdown.execute();
+    }
+
+    public static AoiMain get() {
+        return instance;
+    }
+}
