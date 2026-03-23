5d631941a516af82b949bc80cd05413623643392
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 03:45:39 +0700

    Update CooldownManager.java

--- src/main/java/vn/aoi/onii/skill/CooldownManager.java
+++ src/main/java/vn/aoi/onii/skill/CooldownManager.java
@@ -1,10 +1,11 @@
 package vn.aoi.onii.skill;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CooldownManager {
 
-    private final Map<UUID, Map<String, Long>> cooldowns = new HashMap<>();
+    private final Map<UUID, Map<String, Long>> cooldowns = new ConcurrentHashMap<>();
 
     public boolean isOnCooldown(UUID uuid, String skill) {
         Map<String, Long> map = cooldowns.get(uuid);
@@ -15,7 +16,7 @@ public class CooldownManager {
     }
 
     public void setCooldown(UUID uuid, String skill, long delay) {
-        cooldowns.computeIfAbsent(uuid, k -> new HashMap<>())
+        cooldowns.computeIfAbsent(uuid, k -> new ConcurrentHashMap<>())
                 .put(skill, System.currentTimeMillis() + delay);
     }
 }
