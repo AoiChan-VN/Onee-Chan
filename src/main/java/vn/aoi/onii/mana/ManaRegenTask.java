ef0c9bc10c40f355b815de84be58510b57cf5bc6
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 03:14:53 +0700

    Update ManaRegenTask.java

--- src/main/java/vn/aoi/onii/mana/ManaRegenTask.java
+++ src/main/java/vn/aoi/onii/mana/ManaRegenTask.java
@@ -1,17 +1,17 @@
-// =========================
-//      MANA REGEN TASK
-// =========================
 package vn.aoi.onii.mana;
 
 import org.bukkit.Bukkit;
 import vn.aoi.onii.Main;
+import vn.aoi.onii.player.PlayerData;
 import vn.aoi.onii.player.PlayerManager;
 
 public class ManaRegenTask {
 
     public ManaRegenTask(Main plugin, PlayerManager pm) {
         Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-            pm.getOnline().forEach(data -> data.regenMana(2));
-        }, 40, 40);
+            for (PlayerData d : pm.getOnline()) {
+                d.regenMana(2);
+            }
+        }, 40L, 40L);
     }
-} 
+}
