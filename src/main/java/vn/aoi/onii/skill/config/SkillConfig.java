47e96a1ace638992966b1cfd84e0d9973ce8cd62
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 02:39:48 +0700

    Create SkillConfig.java

---
+++ src/main/java/vn/aoi/onii/skill/config/SkillConfig.java
@@ -0,0 +1,12 @@
+// =========================
+// SKILL CONFIG SYSTEM
+// =========================
+package vn.aoi.onii.skill.config;
+
+public class SkillConfig {
+
+    public String id;
+    public int damage;
+    public int mana;
+    public long cooldown;
+} 
