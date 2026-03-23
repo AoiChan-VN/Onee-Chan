bfc8877e2b42fda05bc5442bae38a7adaf5d0cc2
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 02:58:41 +0700

    Update CommandManager.java

--- src/main/java/vn/aoi/onii/command/CommandManager.java
+++ src/main/java/vn/aoi/onii/command/CommandManager.java
@@ -2,15 +2,12 @@ package vn.aoi.onii.command;
 
 import org.bukkit.command.*;
 import vn.aoi.onii.Main;
-
 import vn.aoi.onii.player.PlayerManager;
-import vn.aoi.onii.classsystem.ClassManager;
 
 public class CommandManager {
 
     private final Main plugin;
     private final PlayerManager playerManager;
-    private final ClassManager ClassManager;
 
     public CommandManager(Main plugin, PlayerManager pm) {
         this.plugin = plugin;
@@ -19,6 +16,5 @@ public class CommandManager {
 
     public void register() {
         plugin.getCommand("onii").setExecutor(new RootCommand(playerManager));
-        plugin.getCommand("class").setExecutor(new ClassCommand(classManager));
     }
 }
