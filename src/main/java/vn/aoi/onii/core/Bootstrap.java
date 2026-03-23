632c778642d33cefa392c4edcb5aa667083e027a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 03:24:31 +0700

    Update Bootstrap.java

--- src/main/java/vn/aoi/onii/core/Bootstrap.java
+++ src/main/java/vn/aoi/onii/core/Bootstrap.java
@@ -1,11 +1,21 @@
 package vn.aoi.onii.core;
 
+import org.bukkit.Bukkit;
 import vn.aoi.onii.Main;
+import vn.aoi.onii.classsystem.*;
+import vn.aoi.onii.classsystem.impl.*;
+import vn.aoi.onii.command.*;
 import vn.aoi.onii.config.ConfigManager;
 import vn.aoi.onii.database.DatabaseManager;
+import vn.aoi.onii.mana.ManaRegenTask;
 import vn.aoi.onii.player.PlayerManager;
 import vn.aoi.onii.scheduler.AsyncExecutor;
-import vn.aoi.onii.command.CommandManager;
+import vn.aoi.onii.skill.*;
+import vn.aoi.onii.skill.config.SkillConfigManager;
+import vn.aoi.onii.skill.impl.*;
+import vn.aoi.onii.skill.listener.SkillListener;
+
+import java.io.File;
 
 public class Bootstrap {
 
@@ -15,25 +25,50 @@ public class Bootstrap {
     private AsyncExecutor executor;
     private DatabaseManager database;
     private PlayerManager playerManager;
-    private CommandManager commandManager;
 
     public Bootstrap(Main plugin) {
         this.plugin = plugin;
     }
 
     public void init() {
-        this.config = new ConfigManager(plugin);
+        config = new ConfigManager(plugin);
         config.loadAll();
 
-        this.executor = new AsyncExecutor();
+        executor = new AsyncExecutor();
 
-        this.database = new DatabaseManager(plugin);
+        database = new DatabaseManager(plugin);
         database.init();
 
-        this.playerManager = new PlayerManager(plugin, database, executor);
+        playerManager = new PlayerManager(plugin, database, executor);
         playerManager.init();
 
-        this.commandManager = new CommandManager(plugin, playerManager);
+        ClassRegistry classRegistry = new ClassRegistry();
+        classRegistry.register(new WarriorClass());
+        classRegistry.register(new MageClass());
+        ClassManager classManager = new ClassManager(playerManager, classRegistry);
+
+        SkillConfigManager skillConfig = new SkillConfigManager();
+        skillConfig.load(new File(plugin.getDataFolder(), "skills.json"));
+
+        SkillRegistry skillRegistry = new SkillRegistry();
+        skillRegistry.register(new WarriorSlash());
+        skillRegistry.register(new MageFireball());
+
+        CooldownManager cooldown = new CooldownManager();
+
+        SkillManager skillManager = new SkillManager(
+                skillRegistry,
+                cooldown,
+                classManager,
+                playerManager,
+                skillConfig
+        );
+
+        Bukkit.getPluginManager().registerEvents(new SkillListener(skillManager, classManager), plugin);
+
+        new ManaRegenTask(plugin, playerManager);
+
+        CommandManager commandManager = new CommandManager(plugin, playerManager, classManager);
         commandManager.register();
     }
 
