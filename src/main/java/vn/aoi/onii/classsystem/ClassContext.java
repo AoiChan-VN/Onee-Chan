978d218fed11aac4a966da598cbd2bb0f71db325
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-25 02:24:45 +0700

    Update ClassContext.java

--- src/main/java/vn/aoi/onii/classsystem/ClassContext.java
+++ src/main/java/vn/aoi/onii/classsystem/ClassContext.java
@@ -2,17 +2,29 @@ package vn.aoi.onii.classsystem;
 
 import org.bukkit.entity.Player;
 import vn.aoi.onii.player.PlayerData;
+import vn.aoi.onii.combat.BuffManager;
 
 public class ClassContext {
 
     private final Player player;
     private final PlayerData data;
+    private final BuffManager buffManager;
 
-    public ClassContext(Player player, PlayerData data) {
+    public ClassContext(Player player, PlayerData data, BuffManager buffManager) {
         this.player = player;
         this.data = data;
+        this.buffManager = buffManager;
     }
 
-    public Player getPlayer() { return player; }
-    public PlayerData getData() { return data; }
+    public Player getPlayer() {
+        return player;
+    }
+
+    public PlayerData getData() {
+        return data;
+    }
+
+    public BuffManager getBuffManager() {
+        return buffManager;
+    }
 }
