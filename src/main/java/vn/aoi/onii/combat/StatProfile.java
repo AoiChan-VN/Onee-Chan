966f9a42cc72328c8f41f384680f0a6468b81649
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 23:43:06 +0700

    Update StatProfile.java

--- src/main/java/vn/aoi/onii/combat/StatProfile.java
+++ src/main/java/vn/aoi/onii/combat/StatProfile.java
@@ -7,20 +7,12 @@ public class StatProfile {
     private final EnumMap<StatType, Double> stats = new EnumMap<>(StatType.class);
 
     public StatProfile() {
-        for (StatType t : StatType.values()) {
-            stats.put(t, 0.0);
-        }
+        for (StatType t : StatType.values()) stats.put(t, 0.0);
     }
 
-    public double get(StatType type) {
-        return stats.get(type);
-    }
+    public double get(StatType type) { return stats.get(type); }
 
-    public void set(StatType type, double value) {
-        stats.put(type, value);
-    }
+    public void set(StatType type, double value) { stats.put(type, value); }
 
-    public void add(StatType type, double value) {
-        stats.put(type, stats.get(type) + value);
-    }
+    public void add(StatType type, double value) { stats.put(type, stats.get(type) + value); }
 }
