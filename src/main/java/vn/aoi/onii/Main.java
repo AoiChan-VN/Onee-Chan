73e1c60d5b64961e0f600a330f862fc1a63cd850
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-25 16:35:34 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,1 +1,24 @@
+package vn.aoi.onii;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        Bukkit.getLogger().info("AoiChan đã thức tỉnh linh lực!");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("AoiChan đã phi thăng...");
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+}
