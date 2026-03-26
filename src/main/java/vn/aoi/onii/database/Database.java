7d7fc2fc266bb2375a90051a000e6d03f7f92f2b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 10:52:10 +0700

    Update Database.java

--- src/main/java/vn/aoi/onii/database/Database.java
+++ src/main/java/vn/aoi/onii/database/Database.java
@@ -1,13 +1,12 @@
 package vn.aoi.onii.database;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.File;
 import java.sql.*;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Database {
 
-    private Connection conn;
+    private Connection connection;
     private final JavaPlugin plugin;
 
     public Database(JavaPlugin plugin) {
@@ -19,7 +18,7 @@ public class Database {
             File file = new File(plugin.getDataFolder(), "data.db");
             if (!file.exists()) plugin.getDataFolder().mkdirs();
 
-            conn = DriverManager.getConnection("jdbc:sqlite:" + file);
+            connection = DriverManager.getConnection("jdbc:sqlite:" + file);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -27,22 +26,29 @@ public class Database {
 
     public void createTable() {
         try {
-            conn.createStatement().executeUpdate("""
-                CREATE TABLE IF NOT EXISTS players (
-                    uuid TEXT PRIMARY KEY,
-                    name TEXT,
-                    realm TEXT,
-                    stage TEXT,
-                    sect TEXT,
-                    technique TEXT
-                )
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("""
+            CREATE TABLE IF NOT EXISTS players (
+                uuid TEXT PRIMARY KEY,
+                name TEXT,
+                realm TEXT,
+                stage TEXT,
+                sect TEXT,
+                technique TEXT
+            )
             """);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public Connection get() {
-        return conn;
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() {
+        try {
+            if (connection != null) connection.close();
+        } catch (Exception ignored) {}
     }
 }
