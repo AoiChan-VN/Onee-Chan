3e44e6e1d8ed256b0829a098a3ffaa90277822b4
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 07:16:08 +0700

    Update PlayerData.java

--- src/main/java/vn/aoi/onii/player/PlayerData.java
+++ src/main/java/vn/aoi/onii/player/PlayerData.java
@@ -1,1 +1,21 @@
+package vn.aoi.onii.player;
 
+public class PlayerData {
+
+    private String name;
+    private String canhGioi;
+    private String tuVi;
+
+    public PlayerData(String name, String canhGioi, String tuVi) {
+        this.name = name;
+        this.canhGioi = canhGioi;
+        this.tuVi = tuVi;
+    }
+
+    public String getName() { return name; }
+    public String getCanhGioi() { return canhGioi; }
+    public String getTuVi() { return tuVi; }
+
+    public void setCanhGioi(String canhGioi) { this.canhGioi = canhGioi; }
+    public void setTuVi(String tuVi) { this.tuVi = tuVi; }
+}
