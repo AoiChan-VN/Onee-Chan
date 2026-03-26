bc35056d6424c6103ca6c020b35090f60b93be53
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 10:19:13 +0700

    Update PlayerManager.java

--- src/main/java/vn/aoi/onii/player/PlayerManager.java
+++ src/main/java/vn/aoi/onii/player/PlayerManager.java
@@ -5,25 +5,25 @@ import vn.aoi.onii.enums.Realm;
 import vn.aoi.onii.enums.Stage;
 
 import java.sql.*;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 public class PlayerManager {
 
     private final Database db;
-    private final HashMap<UUID, PlayerData> cache = new HashMap<>();
+    private final Map<UUID, PlayerData> cache = new HashMap<>();
 
     public PlayerManager(Database db) {
         this.db = db;
     }
 
     public PlayerData get(UUID uuid, String name) {
+
         if (cache.containsKey(uuid)) return cache.get(uuid);
 
         try {
-            PreparedStatement ps = db.getConnection().prepareStatement(
-                    "SELECT * FROM players WHERE uuid=?");
+            PreparedStatement ps = db.get().prepareStatement("SELECT * FROM players WHERE uuid=?");
             ps.setString(1, uuid.toString());
+
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
@@ -31,6 +31,7 @@ public class PlayerManager {
                 data.setRealm(Realm.valueOf(rs.getString("realm")));
                 data.setStage(Stage.valueOf(rs.getString("stage")));
                 data.setSect(rs.getString("sect"));
+                data.setTechnique(rs.getString("technique"));
 
                 cache.put(uuid, data);
                 return data;
@@ -46,18 +47,19 @@ public class PlayerManager {
         return data;
     }
 
-    public void save(PlayerData data) {
+    public void save(PlayerData d) {
         try {
-            PreparedStatement ps = db.getConnection().prepareStatement("""
-                INSERT OR REPLACE INTO players(uuid,name,realm,stage,sect)
-                VALUES(?,?,?,?,?)
+            PreparedStatement ps = db.get().prepareStatement("""
+                INSERT OR REPLACE INTO players(uuid,name,realm,stage,sect,technique)
+                VALUES(?,?,?,?,?,?)
             """);
 
-            ps.setString(1, data.getUuid().toString());
-            ps.setString(2, data.getName());
-            ps.setString(3, data.getRealm().name());
-            ps.setString(4, data.getStage().name());
-            ps.setString(5, data.getSect());
+            ps.setString(1, d.getUuid().toString());
+            ps.setString(2, d.getName());
+            ps.setString(3, d.getRealm().name());
+            ps.setString(4, d.getStage().name());
+            ps.setString(5, d.getSect());
+            ps.setString(6, d.getTechnique());
 
             ps.executeUpdate();
 
