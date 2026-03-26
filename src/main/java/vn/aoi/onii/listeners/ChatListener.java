ac025d1bf7f02369d6ee2fe1f3c96534978156a0
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 07:17:41 +0700

    Update ChatListener.java

--- src/main/java/vn/aoi/onii/listeners/ChatListener.java
+++ src/main/java/vn/aoi/onii/listeners/ChatListener.java
@@ -1,1 +1,24 @@
+package vn.aoi.onii.listeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import vn.aoi.onii.player.PlayerData;
+import vn.aoi.onii.player.PlayerManager;
+
+public class ChatListener implements Listener {
+
+    private final PlayerManager manager;
+
+    public ChatListener(PlayerManager manager) {
+        this.manager = manager;
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        PlayerData data = manager.getPlayer(e.getPlayer().getUniqueId(), e.getPlayer().getName());
+
+        String format = "§7[ §e" + data.getCanhGioi() + " §7] §f" + e.getPlayer().getName() + ": " + e.getMessage();
+        e.setFormat(format);
+    }
+}
