cb142b93ce2e5e2e63cd8b4093ff491401d07d3b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 10:04:31 +0700

    Update AoiCommand.java

--- src/main/java/vn/aoi/onii/commands/AoiCommand.java
+++ src/main/java/vn/aoi/onii/commands/AoiCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import vn.aoi.onii.player.PlayerData;
 import vn.aoi.onii.player.PlayerManager;
+import vn.aoi.onii.shop.ShopManager;
 
 public class AoiCommand implements CommandExecutor {
 
@@ -17,13 +18,17 @@ public class AoiCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        if (!(sender instanceof Player player)) return true;
+
+        if (args.length == 1 && args[0].equalsIgnoreCase("shop")) {
+            player.openInventory(new ShopManager().createShop());
+            return true;
+        }
+
         if (args.length == 2 && args[0].equalsIgnoreCase("info")) {
 
             Player target = Bukkit.getPlayer(args[1]);
-            if (target == null) {
-                sender.sendMessage("Không tìm thấy người chơi");
-                return true;
-            }
+            if (target == null) return true;
 
             PlayerData data = manager.get(target.getUniqueId(), target.getName());
 
@@ -35,7 +40,7 @@ public class AoiCommand implements CommandExecutor {
             return true;
         }
 
-        sender.sendMessage("/aoi info <player>");
+        player.sendMessage("/aoi shop | /aoi info <player>");
         return true;
     }
 }
