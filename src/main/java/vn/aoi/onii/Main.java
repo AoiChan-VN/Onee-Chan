043eb2846a09189d94c744c58c60917984e35a8a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 09:06:00 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -2,14 +2,14 @@ package vn.aoi.onii;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import vn.aoi.onii.commands.AoiCommand;
-import vn.aoi.onii.database.SQLite;
+import vn.aoi.onii.database.Database;
 import vn.aoi.onii.listeners.ChatListener;
 import vn.aoi.onii.player.PlayerManager;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
-    private SQLite sqlite;
+    private Database database;
     private PlayerManager playerManager;
 
     @Override
@@ -18,24 +18,24 @@ public class Main extends JavaPlugin {
 
         saveDefaultConfig();
 
-        sqlite = new SQLite(this);
-        sqlite.connect();
-        sqlite.createTable();
+        database = new Database(this);
+        database.connect();
+        database.createTable();
 
-        playerManager = new PlayerManager(sqlite);
+        playerManager = new PlayerManager(database);
 
         getCommand("aoi").setExecutor(new AoiCommand(playerManager));
-
         getServer().getPluginManager().registerEvents(new ChatListener(playerManager), this);
 
         saveResource("shop.yml", false);
     }
 
-    public static Main getInstance() {
-        return instance;
+    @Override
+    public void onDisable() {
+        database.close();
     }
 
-    public SQLite getSQLite() {
-        return sqlite;
+    public static Main getInstance() {
+        return instance;
     }
 }
