0dc31ef3b2f6edf2176296e2d1085cb4a64c5ae8
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 09:08:31 +0700

    Update PlayerData.java

--- src/main/java/vn/aoi/onii/player/PlayerData.java
+++ src/main/java/vn/aoi/onii/player/PlayerData.java
@@ -1,21 +1,35 @@
 package vn.aoi.onii.player;
 
+import vn.aoi.onii.enums.Realm;
+import vn.aoi.onii.enums.Stage;
+
+import java.util.UUID;
+
 public class PlayerData {
 
+    private UUID uuid;
     private String name;
-    private String canhGioi;
-    private String tuVi;
+    private Realm realm;
+    private Stage stage;
+    private String sect;
 
-    public PlayerData(String name, String canhGioi, String tuVi) {
+    public PlayerData(UUID uuid, String name) {
+        this.uuid = uuid;
         this.name = name;
-        this.canhGioi = canhGioi;
-        this.tuVi = tuVi;
+        this.realm = Realm.PHAM_NHAN;
+        this.stage = Stage.SO_KY;
+        this.sect = "Không có";
     }
 
+    public UUID getUuid() { return uuid; }
     public String getName() { return name; }
-    public String getCanhGioi() { return canhGioi; }
-    public String getTuVi() { return tuVi; }
 
-    public void setCanhGioi(String canhGioi) { this.canhGioi = canhGioi; }
-    public void setTuVi(String tuVi) { this.tuVi = tuVi; }
+    public Realm getRealm() { return realm; }
+    public void setRealm(Realm realm) { this.realm = realm; }
+
+    public Stage getStage() { return stage; }
+    public void setStage(Stage stage) { this.stage = stage; }
+
+    public String getSect() { return sect; }
+    public void setSect(String sect) { this.sect = sect; }
 }
