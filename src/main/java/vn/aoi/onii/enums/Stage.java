d7d11e970a6b6383dc37617d2fc3e7175a78249b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 09:11:35 +0700

    Create Stage.java

---
+++ src/main/java/vn/aoi/onii/enums/Stage.java
@@ -0,0 +1,18 @@
+package vn.aoi.onii.enums;
+
+public enum Stage {
+    SO_KY("Sơ Kỳ"),
+    TRUNG_KY("Trung Kỳ"),
+    HAU_KY("Hậu Kỳ"),
+    DINH_PHONG("Đỉnh Phong");
+
+    private final String display;
+
+    Stage(String display) {
+        this.display = display;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+} 
