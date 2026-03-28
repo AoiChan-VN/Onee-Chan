6ccac19240dc814f717e239370fa2df0b0a2f4c0
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 06:48:30 +0700

    Add getPermission method to SubCommand interface

--- src/main/java/vn/aoi/onii/commands/core/SubCommand.java
+++ src/main/java/vn/aoi/onii/commands/core/SubCommand.java
@@ -1,13 +1,13 @@
 package vn.aoi.onii.commands.core;
 
 import org.bukkit.command.CommandSender;
-
 import java.util.List;
 
 public interface SubCommand {
     String getName();
     String getDescription();
     String getUsage();
+    String getPermission();
 
     boolean execute(CommandSender sender, String[] args);
 
