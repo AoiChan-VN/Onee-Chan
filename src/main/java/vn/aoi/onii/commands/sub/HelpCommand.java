cf9b4c30a2ef1a89b33dc184b79135cd4b19ffb7
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 06:37:21 +0700

    Update HelpCommand.java

--- src/main/java/vn/aoi/onii/commands/sub/HelpCommand.java
+++ src/main/java/vn/aoi/onii/commands/sub/HelpCommand.java
@@ -25,8 +25,19 @@ public class HelpCommand implements SubCommand {
 
     @Override
     public boolean execute(CommandSender sender, String[] args) {
-        sender.sendMessage("§a=== AOI COMMANDS ===");
+        sender.sendMessage("§a=== Hướng Dẫn Lệnh ===");
         sender.sendMessage("§e/aoi help §7- Show help");
+        sender.sendMessage("§e/aoi profile §7→ Thông tin");
+        sender.sendMessage("§e/aoi top §7→ BXH");
+        sender.sendMessage("§e/aoi break §7→ Đột phá");
+        
+        sender.sendMessage("§c§l--- ADMIN ---");
+        sender.sendMessage("§e/aoi setexp <p> <amount>");
+        sender.sendMessage("§e/aoi addexp <p> <amount>");
+        sender.sendMessage("§e/aoi setrealm <p> <realm>");
+        sender.sendMessage("§e/aoi setstage <p> <stage>");
+        sender.sendMessage("§e/aoi settech <p> <tier>");
+        sender.sendMessage("§e/aoi reload");
         return true;
     }
 
