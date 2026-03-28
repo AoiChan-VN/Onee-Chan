1ad2ee8dd374ae93ecc0d5663978c797434e5118
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 01:53:00 +0700

    Refactor Main class to improve economy handling

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,80 +1,126 @@
 package vn.aoi.onii;
 
 import net.milkbowl.vault.economy.Economy;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import vn.aoi.onii.commands.*;
 import vn.aoi.onii.database.Database;
 import vn.aoi.onii.listeners.ChatListener;
 import vn.aoi.onii.player.PlayerManager;
-import vn.aoi.onii.quest.*;
+import vn.aoi.onii.quest.QuestManager;
 import vn.aoi.onii.shop.ShopListener;
-
-import java.io.File;
+import vn.aoi.onii.quest.QuestListener;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
+
     private Database database;
     private PlayerManager playerManager;
-    private Economy econ;
     private QuestManager questManager;
+    private Economy economy;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        saveDefaultConfig();
-        saveResource("shop.yml", false);
-        saveResource("quests.yml", false);
+        loadConfigs();
 
-        setupEconomy();
+        if (!setupEconomy()) {
+            getLogger().severe("Vault not found! Disabling plugin...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
-        database = new Database(this);
-        database.connect();
-        database.createTable();
+        initDatabase();
+        initManagers();
+        registerCommands();
+        registerListeners();
 
-        playerManager = new PlayerManager(database);
-        questManager = new QuestManager();
-
-        getCommand("aoi").setExecutor(new AoiCommand(playerManager));
-        getCommand("aoi").setTabCompleter(new AoiTabComplete());
-        
-        getServer().getPluginManager().registerEvents(new ChatListener(playerManager), this);
-        getServer().getPluginManager().registerEvents(new ShopListener(playerManager, econ), this);
-        getServer().getPluginManager().registerEvents(new QuestListener(questManager, playerManager, econ), this);
+        getLogger().info("Onii plugin enabled successfully!");
     }
 
     @Override
     public void onDisable() {
-        database.close();
+        if (database != null) {
+            database.close();
+        }
+        getLogger().info("Onii plugin disabled.");
     }
+
+    // ===================== INIT SECTIONS =====================
+
+    private void loadConfigs() {
+        saveDefaultConfig();
+        saveResource("shop.yml", false);
+        saveResource("quests.yml", false);
+    }
+
+    private void initDatabase() {
+        database = new Database(this);
+        database.connect();
+        database.createTable();
+    }
+
+    private void initManagers() {
+        playerManager = new PlayerManager(database);
+        questManager = new QuestManager();
+    }
+
+    private void registerCommands() {
+        PluginCommand cmd = getCommand("aoi");
+        if (cmd == null) {
+            getLogger().severe("Command 'aoi' not found in plugin.yml!");
+            return;
+        }
+
+        cmd.setExecutor(new AoiCommand(playerManager));
+        cmd.setTabCompleter(new AoiTabComplete());
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new ChatListener(playerManager), this);
+        getServer().getPluginManager().registerEvents(new ShopListener(playerManager, economy), this);
+        getServer().getPluginManager().registerEvents(new QuestListener(questManager, playerManager, economy), this);
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp =
+                getServer().getServicesManager().getRegistration(Economy.class);
+
+        if (rsp == null) {
+            return false;
+        }
+
+        economy = rsp.getProvider();
+        return economy != null;
+    }
+
+    // ===================== GETTERS =====================
 
     public static Main getInstance() {
         return instance;
     }
 
-    public File getShopFile() {
-        return new File(getDataFolder(), "shop.yml");
-    }
-
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) return false;
-
-        RegisteredServiceProvider<Economy> rsp =
-                getServer().getServicesManager().getRegistration(Economy.class);
-
-        if (rsp == null) return false;
-
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-
-    public Economy getEcon() {
-        return econ;
-    }
-
     public Database getDatabase() {
         return database;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+
+    public PlayerManager getPlayerManager() {
+        return playerManager;
+    }
+
+    public QuestManager getQuestManager() {
+        return questManager;
     }
 }
