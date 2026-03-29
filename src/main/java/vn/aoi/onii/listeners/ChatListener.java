d624a3efc74126e8a2a56dfa3b94354b293bc48c
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 20:20:02 +0700

    Update ChatListener.java

--- src/main/java/vn/aoi/onii/listeners/ChatListener.java
+++ src/main/java/vn/aoi/onii/listeners/ChatListener.java
@@ -1,22 +1,27 @@
 package vn.aoi.onii.listeners;
 
-import org.bukkit.event.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import vn.aoi.onii.player.PlayerManager;
+import vn.aoi.onii.player.PlayerData;
 
 public class ChatListener implements Listener {
 
-    private final PlayerManager manager;
+    private final PlayerManager playerManager;
 
-    public ChatListener(PlayerManager manager) {
-        this.manager = manager;
+    public ChatListener(PlayerManager playerManager) {
+        this.playerManager = playerManager;
     }
 
     @EventHandler
-    public void chat(AsyncPlayerChatEvent e) {
-        var data = manager.get(e.getPlayer().getUniqueId(), e.getPlayer().getName());
+    public void onChat(AsyncPlayerChatEvent e) {
+        PlayerData data = playerManager.get(e.getPlayer());
 
-        e.setFormat("[ " + data.getRealm().getDisplay() + " ] " +
-                e.getPlayer().getName() + ": " + e.getMessage());
+        if (data == null) return;
+
+        e.setFormat("[" + data.getRealm() + "] " +
+                e.getPlayer().getName() + ": " +
+                e.getMessage());
     }
 }
