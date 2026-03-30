4be24b0623bf1a98f42510f0ed54a6d3171189d4
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-30 07:14:25 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,20 +1,17 @@
 package vn.aoi.onii;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import vn.aoi.onii.command.TutienCommand;
+import vn.aoi.onii.player.PlayerManager;
+import vn.aoi.onii.cultivation.CultivationService;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.logging.Level;
-
-public final class Main extends JavaPlugin {
+public class Main extends JavaPlugin {
 
     private static Main instance;
-    private FileConfiguration config;
+    private PlayerManager playerManager;
+    private CultivationService cultivationService;
 
-    public static Main getInstance() {
+    public static Main get() {
         return instance;
     }
 
@@ -23,46 +20,15 @@ public final class Main extends JavaPlugin {
         instance = this;
 
         saveDefaultConfig();
-        config = getConfig();
 
-        Bukkit.getLogger().info("[OniiCore] Plugin Enabled!");
+        playerManager = new PlayerManager();
+        cultivationService = new CultivationService(playerManager);
+
+        getCommand("tutien").setExecutor(new TutienCommand(playerManager, cultivationService));
+
+        getLogger().info("OniiTuTien Enabled!");
     }
 
-    @Override
-    public void onDisable() {
-        Bukkit.getLogger().info("[OniiCore] Plugin Disabled!");
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!command.getName().equalsIgnoreCase("onii")) return false;
-
-        if (!sender.hasPermission("onii.admin")) {
-            sender.sendMessage(color("&cNo permission."));
-            return true;
-        }
-
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            reloadAsync(sender);
-            return true;
-        }
-
-        sender.sendMessage(color("&eUsage: /onii reload"));
-        return true;
-    }
-
-    private void reloadAsync(CommandSender sender) {
-        CompletableFuture.runAsync(() -> {
-            try {
-                reloadConfig();
-                config = getConfig();
-            } catch (Exception e) {
-                Bukkit.getLogger().log(Level.SEVERE, "Reload failed", e);
-            }
-        }).thenRun(() -> sender.sendMessage(color(config.getString("messages.reload"))));
-    }
-
-    public String color(String msg) {
-        return msg.replace("&", "§");
-    }
+    public PlayerManager getPlayerManager() { return playerManager; }
+    public CultivationService getCultivationService() { return cultivationService; }
 }
