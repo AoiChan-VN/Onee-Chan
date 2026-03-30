8bd25e1da07b7ad44400c0c2e23f76c82f795e3b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-30 11:56:55 +0700

    Create RankUtil.java

---
+++ src/main/java/vn/aoi/onii/util/RankUtil.java
@@ -0,0 +1,14 @@
+package vn.aoi.onii.util;
+
+public class RankUtil {
+
+    public static final String[] RANKS = {
+        "Phàm nhân","Luyện khí","Trúc cơ","Kim đan","Nguyên anh",
+        "Hóa thần","Luyện hư","Hợp thể","Đại thừa","Độ kiếp",
+        "Chân tiên","Kim tiên","Thái ất","Đại la","Đạo tổ","Siêu thoát"
+    };
+
+    public static final String[] PHASES = {
+        "Sơ kỳ","Trung kỳ","Hậu kỳ","Viên mãn"
+    };
+} 
