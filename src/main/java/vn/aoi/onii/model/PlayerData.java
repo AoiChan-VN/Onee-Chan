2980f28c7d19e738c5617fdd2fe7a1ff3fe0c331
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-30 11:55:18 +0700

    Create PlayerData.java

---
+++ src/main/java/vn/aoi/onii/model/PlayerData.java
@@ -0,0 +1,13 @@
+package vn.aoi.onii.model;
+
+public class PlayerData {
+    public int rank;
+    public int phase;
+    public int level;
+
+    public PlayerData(int rank, int phase, int level) {
+        this.rank = rank;
+        this.phase = phase;
+        this.level = level;
+    }
+} 
