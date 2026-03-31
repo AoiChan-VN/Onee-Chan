422431a0f0c0c9058e30c23619246623076854eb
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-31 08:37:39 +0700

    Create PlayerData.java

---
+++ src/main/java/vn/aoi/onii/player/PlayerData.java
@@ -0,0 +1,21 @@
+package vn.aoi.onii.player;
+
+public class PlayerData {
+    private String rank;
+    private int level;
+    private int exp;
+
+    public PlayerData(String rank, int level, int exp) {
+        this.rank = rank;
+        this.level = level;
+        this.exp = exp;
+    }
+
+    public String getRank() { return rank; }
+    public int getLevel() { return level; }
+    public int getExp() { return exp; }
+
+    public void setRank(String rank) { this.rank = rank; }
+    public void setLevel(int level) { this.level = level; }
+    public void setExp(int exp) { this.exp = exp; }
+} 
