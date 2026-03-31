e323d5c516a05687d3a12f6583ff3f1e536c884f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 06:46:35 +0700

    Update Database.java

--- src/main/java/vn/aoi/onii/data/Database.java
+++ src/main/java/vn/aoi/onii/data/Database.java
@@ -1,13 +1,13 @@
 package vn.aoi.onii.data;
 
-import vn.aoi.onii.AoiMain;
+import vn.aoi.onii.AoiChanPlugin;
 import java.sql.*;
 
 public class Database {
-    private final AoiMain plugin;
+    private final AoiChanPlugin plugin;
     private Connection conn;
 
-    public Database(AoiMain plugin) { this.plugin = plugin; }
+    public Database(AoiChanPlugin plugin) { this.plugin = plugin; }
 
     public void init() {
         try {
