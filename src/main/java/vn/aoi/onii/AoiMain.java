bf90502d3a8694f0fa6a382f60f8283a5c8c508f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 06:03:22 +0700

    Update AoiMain.java

--- src/main/java/vn/aoi/onii/AoiMain.java
+++ src/main/java/vn/aoi/onii/AoiMain.java
@@ -1,32 +1,41 @@
 package vn.aoi.onii;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import vn.aoi.onii.data.Database;
+import vn.aoi.onii.listener.PlayerListener;
 import vn.aoi.onii.player.PlayerManager;
 import vn.aoi.onii.rank.RankManager;
+import vn.aoi.onii.system.ExpService;
+import vn.aoi.onii.system.DoKiepService;
 
-public final class AoiMain extends JavaPlugin {
+public final class AoiChanPlugin extends JavaPlugin {
 
-    private static AoiMain instance;
+    private static AoiChanPlugin instance;
     private Database database;
     private PlayerManager playerManager;
     private RankManager rankManager;
+    private ExpService expService;
+    private DoKiepService doKiepService;
 
     @Override
     public void onEnable() {
         instance = this;
-
         saveDefaultConfig();
 
-        this.database = new Database(this);
+        database = new Database(this);
         database.init();
 
-        this.rankManager = new RankManager(this);
+        rankManager = new RankManager(this);
         rankManager.load();
 
-        this.playerManager = new PlayerManager(this);
+        playerManager = new PlayerManager(this);
+        expService = new ExpService(this);
+        doKiepService = new DoKiepService(this);
 
-        getLogger().info("Plugins ➝【Bật】");
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
+
+        getLogger().info("AoiChan FULL Enabled!");
     }
 
     @Override
@@ -35,19 +44,10 @@ public final class AoiMain extends JavaPlugin {
         database.close();
     }
 
-    public static AoiMain get() {
-        return instance;
-    }
-
-    public Database getDatabase() {
-        return database;
-    }
-
-    public PlayerManager getPlayerManager() {
-        return playerManager;
-    }
-
-    public RankManager getRankManager() {
-        return rankManager;
-    }
+    public static AoiChanPlugin get() { return instance; }
+    public Database getDatabase() { return database; }
+    public PlayerManager getPlayerManager() { return playerManager; }
+    public RankManager getRankManager() { return rankManager; }
+    public ExpService getExpService() { return expService; }
+    public DoKiepService getDoKiepService() { return doKiepService; }
 }
