07c3c23aa87d5477fd0c7c0df84ff328f984d9b7
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 23:32:11 +0700

    Update Main.java

--- src/main/java/aoi/onii/Main.java
+++ src/main/java/aoi/onii/Main.java
@@ -1,1 +1,43 @@
+package vn.aoi.onii;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import vn.aoi.onii.command.CommandManager;
+import vn.aoi.onii.config.ConfigManager;
+import vn.aoi.onii.database.DatabaseManager;
+import vn.aoi.onii.listener.MobKillListener;
+import vn.aoi.onii.service.CultivationService;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+    private DatabaseManager databaseManager;
+    private ConfigManager configManager;
+    private CultivationService cultivationService;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        this.configManager = new ConfigManager(this);
+        this.configManager.loadAll();
+
+        this.databaseManager = new DatabaseManager(this);
+        this.databaseManager.init();
+
+        this.cultivationService = new CultivationService(this);
+
+        new CommandManager(this).registerAll();
+
+        getServer().getPluginManager().registerEvents(new MobKillListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        databaseManager.shutdown();
+    }
+
+    public static Main getInstance() { return instance; }
+    public DatabaseManager getDatabaseManager() { return databaseManager; }
+    public ConfigManager getConfigManager() { return configManager; }
+    public CultivationService getCultivationService() { return cultivationService; }
+}
