235890bfc4d54eaa850d473c0689cf344924117c
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 11:30:57 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,16 +1,18 @@
 package vn.aoi.onii;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import vn.aoi.onii.database.DatabaseManager;
-import vn.aoi.onii.player.PlayerManager;
-import vn.aoi.onii.cultivation.realms.RealmManager;
+import vn.aoi.onii.commands.CommandManager;
+import vn.aoi.onii.config.MessageManager;
 import vn.aoi.onii.cultivation.CultivationService;
-import vn.aoi.onii.commands.CultivationCommand;
+import vn.aoi.onii.cultivation.RealmManager;
+import vn.aoi.onii.database.DatabaseManager;
 import vn.aoi.onii.listeners.PlayerListener;
+import vn.aoi.onii.player.PlayerManager;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
+
     private DatabaseManager databaseManager;
     private PlayerManager playerManager;
     private RealmManager realmManager;
@@ -25,14 +27,19 @@ public class Main extends JavaPlugin {
         databaseManager = new DatabaseManager(this);
         databaseManager.init();
 
+        MessageManager.init(this);
+
         realmManager = new RealmManager(this);
         realmManager.loadRealms();
 
         playerManager = new PlayerManager(this);
-
         cultivationService = new CultivationService(this);
 
-        getCommand("tuvi").setExecutor(new CultivationCommand(this));
+        try {
+            new CommandManager(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
     }
@@ -42,23 +49,9 @@ public class Main extends JavaPlugin {
         playerManager.saveAll();
     }
 
-    public static Main getInstance() {
-        return instance;
-    }
-
-    public DatabaseManager getDatabaseManager() {
-        return databaseManager;
-    }
-
-    public PlayerManager getPlayerManager() {
-        return playerManager;
-    }
-
-    public RealmManager getRealmManager() {
-        return realmManager;
-    }
-
-    public CultivationService getCultivationService() {
-        return cultivationService;
-    }
+    public static Main getInstance() { return instance; }
+    public DatabaseManager getDatabaseManager() { return databaseManager; }
+    public PlayerManager getPlayerManager() { return playerManager; }
+    public RealmManager getRealmManager() { return realmManager; }
+    public CultivationService getCultivationService() { return cultivationService; }
 }
