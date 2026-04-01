98ae866ccf424b4de20675ce0e02aed5e4df4857
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 12:00:02 +0700

    Update InfoCommand.java

--- src/main/java/vn/aoi/onii/commands/modules/InfoCommand.java
+++ src/main/java/vn/aoi/onii/commands/modules/InfoCommand.java
@@ -1,13 +1,17 @@
 package vn.aoi.onii.commands.modules;
 
 import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.annotations.CommandDescription;
 import org.bukkit.entity.Player;
+import vn.aoi.onii.Main;
 import vn.aoi.onii.commands.framework.AbstractCommand;
+import vn.aoi.onii.commands.framework.annotations.CommandCooldown;
 import vn.aoi.onii.config.MessageManager;
+import vn.aoi.onii.player.PlayerData;
 
 public class InfoCommand extends AbstractCommand {
 
-    public InfoCommand(vn.aoi.onii.Main plugin) {
+    public InfoCommand(Main plugin) {
         super(plugin);
     }
 
@@ -17,9 +21,15 @@ public class InfoCommand extends AbstractCommand {
     }
 
     @CommandMethod("aoi info")
+    @CommandDescription("Xem thông tin tu vi")
+    @CommandCooldown(seconds = 3)
     public void info(Player player) {
 
-        var data = plugin.getPlayerManager().get(player);
+        PlayerData data = plugin.getPlayerManager().get(player);
+        if (data == null) {
+            player.sendMessage("§cKhông tìm thấy dữ liệu!");
+            return;
+        }
 
         player.sendMessage(MessageManager.get("info.header"));
         player.sendMessage(MessageManager.get("info.realm")
