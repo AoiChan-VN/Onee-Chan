b9b2489a1dc3989cb05b01e9bfa2aabdb305f476
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 00:11:30 +0700

    Delete src/main/java/aoi/onii directory

--- src/main/java/aoi/onii/command/BaseCommand.java
+++
@@ -1,8 +0,0 @@
-package vn.aoi.onii.command;
-
-import org.bukkit.command.CommandSender;
-
-public abstract class BaseCommand {
-    public abstract String getName();
-    public abstract void execute(CommandSender sender, String[] args);
-} 
