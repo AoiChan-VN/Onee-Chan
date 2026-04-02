086e914c8203b043ad34b6a29f42aae9c0d6f834
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-02 23:33:43 +0700

    Create PlayerRepository.java

---
+++ src/main/java/aoi/onii/data/PlayerRepository.java
@@ -0,0 +1,29 @@
+package vn.aoi.onii.data;
+
+import org.bukkit.Bukkit;
+import vn.aoi.onii.Main;
+
+import java.sql.PreparedStatement;
+
+public class PlayerRepository {
+
+    public static void saveAsync(PlayerData data) {
+
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> {
+            try {
+                PreparedStatement ps = Main.getInstance().getDatabase().getConnection()
+                        .prepareStatement("UPDATE players SET realm=?, level=?, exp=? WHERE uuid=?");
+
+                ps.setString(1, data.getRealm());
+                ps.setInt(2, data.getLevel());
+                ps.setInt(3, data.getExp());
+                ps.setString(4, data.getUuid().toString());
+
+                ps.executeUpdate();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+} 
