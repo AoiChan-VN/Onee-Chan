523e2ae7f0812f6cbdf916cba56fb831e4026eaa
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 01:04:29 +0700

    Update MobKillListener.java

--- src/main/java/vn/aoi/onii/listener/MobKillListener.java
+++ src/main/java/vn/aoi/onii/listener/MobKillListener.java
@@ -4,22 +4,22 @@ import org.bukkit.event.*;
 import org.bukkit.event.entity.EntityDeathEvent;
 import vn.aoi.onii.config.ConfigManager;
 import vn.aoi.onii.manager.*;
+import vn.aoi.onii.data.PlayerData;
 
 public class MobKillListener implements Listener {
 
     @EventHandler
-    public void kill(EntityDeathEvent e) {
+    public void kill(EntityDeathEvent e){
+        var p=e.getEntity().getKiller();
+        if(p==null) return;
 
-        var p = e.getEntity().getKiller();
-        if (p == null) return;
+        String type=e.getEntity().getType().name();
+        if(!ConfigManager.mobs.contains(type)) return;
 
-        var type = e.getEntity().getType().name();
-        if (!ConfigManager.mobs.contains(type)) return;
+        PlayerData d=PlayerManager.get(p.getUniqueId());
+        if(d==null) return;
 
-        var data = PlayerManager.get(p.getUniqueId());
-        if (data == null) return;
-
-        data.addExp(ConfigManager.mobs.getInt(type));
-        LevelManager.check(p, data);
+        d.exp+=ConfigManager.mobs.getInt(type);
+        LevelManager.check(p,d);
     }
-} 
+}
