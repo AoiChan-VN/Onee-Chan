b9ccba8510a7fc286cf8a333491a9f8ce366cb81
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 00:59:25 +0700

    Update RealmManager.java

--- src/main/java/vn/aoi/onii/manager/RealmManager.java
+++ src/main/java/vn/aoi/onii/manager/RealmManager.java
@@ -1,41 +1,19 @@
 package vn.aoi.onii.manager;
 
 import vn.aoi.onii.config.ConfigManager;
-import vn.aoi.onii.model.*;
-
 import java.util.*;
 
 public class RealmManager {
 
-    private static final Map<String, Realm> map = new HashMap<>();
+    private static final Map<String,Object> map=new HashMap<>();
 
-    public static void load() {
+    public static void load(){
         map.clear();
-
-        var root = ConfigManager.realms.getConfigurationSection("realms");
-        if (root == null) return;
-
-        for (String key : root.getKeys(false)) {
-
-            int max = root.getInt(key + ".max-level");
-            String next = root.getString(key + ".next-rank");
-            boolean trib = root.getBoolean(key + ".is-thien-kiep");
-
-            Map<Integer, LevelInfo> levels = new HashMap<>();
-
-            var sec = root.getConfigurationSection(key + ".levels");
-            if (sec != null) {
-                for (String l : sec.getKeys(false)) {
-                    levels.put(Integer.parseInt(l),
-                            new LevelInfo(sec.getInt(l + ".exp")));
-                }
-            }
-
-            map.put(key, new Realm(key, max, levels, next, trib));
-        }
+        var sec=ConfigManager.realms.getConfigurationSection("realms");
+        if(sec!=null) for(String k:sec.getKeys(false)) map.put(k,new Object());
     }
 
-    public static Realm get(String name) {
-        return map.get(name);
+    public static boolean exist(String name){
+        return map.containsKey(name);
     }
-} 
+}
