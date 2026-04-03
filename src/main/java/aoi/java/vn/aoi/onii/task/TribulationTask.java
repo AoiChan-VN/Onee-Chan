c60cbcc790e2ab15525aad62fa5c27c96bb6d0b6
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 09:55:41 +0700

    Update TribulationTask.java

--- src/main/java/aoi/java/vn/aoi/onii/task/TribulationTask.java
+++ src/main/java/aoi/java/vn/aoi/onii/task/TribulationTask.java
@@ -1,9 +1,9 @@
 package vn.aoi.onii.task;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import vn.aoi.onii.manager.CultivationService;
 import vn.aoi.onii.manager.PlayerManager;
 import vn.aoi.onii.model.Cultivator;
 
@@ -13,19 +13,26 @@ public class TribulationTask extends BukkitRunnable {
 
     private final Player player;
     private final PlayerManager playerManager;
+    private final CultivationService cultivationService;
     private final int duration;
 
     private int time = 0;
     private final Random random = new Random();
 
-    public TribulationTask(Player player, PlayerManager playerManager, int duration) {
+    public TribulationTask(Player player,
+                           PlayerManager playerManager,
+                           int duration,
+                           CultivationService cultivationService) {
+
         this.player = player;
         this.playerManager = playerManager;
         this.duration = duration;
+        this.cultivationService = cultivationService;
     }
 
     @Override
     public void run() {
+
         if (!player.isOnline() || player.isDead()) {
             fail();
             cancel();
@@ -54,12 +61,23 @@ public class TribulationTask extends BukkitRunnable {
         if (c != null) {
             c.setExp(0);
         }
+
+        player.sendMessage("§4Bạn đã thất bại thiên kiếp!");
     }
 
     private void success() {
         Cultivator c = playerManager.get(player.getUniqueId());
-        if (c != null) {
-            // TODO: rank up
-        }
+        if (c == null) return;
+
+        String next = cultivationService
+                .getRealmManager()
+                .getRealm(c.getRealm())
+                .getNextRank();
+
+        c.setRealm(next);
+        c.setLevel(1);
+        c.setExp(0);
+
+        player.sendMessage("§b⚡ Bạn đã vượt qua thiên kiếp!");
     }
-} 
+}
