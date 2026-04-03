c4cbef486ce564516febf02389de11af74706118
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 07:51:57 +0700

    Create CultivationService.java

---
+++ src/main/java/aoi/java/vn/aoi/onii/manager/CultivationService.java
@@ -0,0 +1,66 @@
+package vn.aoi.onii.manager;
+
+import vn.aoi.onii.model.Cultivator;
+import vn.aoi.onii.model.Realm;
+
+public class CultivationService {
+
+    private final PlayerManager playerManager;
+    private final RealmManager realmManager;
+
+    public CultivationService(PlayerManager playerManager, RealmManager realmManager) {
+        this.playerManager = playerManager;
+        this.realmManager = realmManager;
+    }
+
+    // ➕ ADD EXP
+    public void addExp(Cultivator cultivator, double amount) {
+        if (amount <= 0) return;
+
+        cultivator.setExp(cultivator.getExp() + amount);
+
+        checkLevelUp(cultivator);
+    }
+
+    // 🔼 LEVEL UP LOGIC
+    private void checkLevelUp(Cultivator cultivator) {
+        Realm realm = realmManager.getRealm(cultivator.getRealm());
+
+        if (realm == null) return;
+
+        while (true) {
+            int level = cultivator.getLevel();
+
+            // MAX → RANK UP
+            if (level >= realm.getMaxLevel()) {
+                if (realm.getNextRank() != null) {
+                    handleRankUp(cultivator, realm);
+                }
+                break;
+            }
+
+            Realm.LevelData data = realm.getLevels().get(level);
+
+            if (data == null) break;
+
+            if (cultivator.getExp() >= data.getExpRequired()) {
+                cultivator.setExp(cultivator.getExp() - data.getExpRequired());
+                cultivator.setLevel(level + 1);
+            } else {
+                break;
+            }
+        }
+    }
+
+    // 🌩️ RANK UP
+    private void handleRankUp(Cultivator cultivator, Realm realm) {
+        if (realm.isTribulation()) {
+            // TODO: trigger TribulationTask
+            return;
+        }
+
+        cultivator.setRealm(realm.getNextRank());
+        cultivator.setLevel(1);
+        cultivator.setExp(0);
+    }
+}
