40e4cea21123beeb89f06bddd73448d9bb617329
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 10:12:53 +0700

    Create InfoCommand.java

---
+++ src/main/java/aoi/java/vn/aoi/onii/commands/InfoCommand.java
@@ -0,0 +1,40 @@
+package vn.aoi.onii.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import vn.aoi.onii.manager.PlayerManager;
+import vn.aoi.onii.model.Cultivator;
+
+public class CultivationCommand implements CommandExecutor {
+
+    private final PlayerManager playerManager;
+
+    public CultivationCommand(PlayerManager playerManager) {
+        this.playerManager = playerManager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only player!");
+            return true;
+        }
+
+        Cultivator c = playerManager.get(player.getUniqueId());
+
+        if (c == null) {
+            player.sendMessage("§cChưa load dữ liệu!");
+            return true;
+        }
+
+        player.sendMessage("§6=== TU VI ===");
+        player.sendMessage("§eCảnh giới: §f" + c.getRealm());
+        player.sendMessage("§eCấp độ: §f" + c.getLevel());
+        player.sendMessage("§eEXP: §f" + c.getExp());
+
+        return true;
+    }
+}
