78d151500d6a845d06510aed2dec17e7428992ca
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-06 21:56:45 +0700

    Update ConfigManager.java

--- src/main/java/vn/aoi/onii/config/ConfigManager.java
+++ src/main/java/vn/aoi/onii/config/ConfigManager.java
@@ -1,26 +1,59 @@
 package vn.aoi.onii.config;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.YamlConfiguration;
+import vn.aoi.onii.AoiPlugin;
+
+import java.io.File;
 
 public class ConfigManager {
 
-    private final JavaPlugin plugin;
+    private final AoiPlugin plugin;
 
-    public ConfigManager(JavaPlugin plugin) {
+    private FileConfiguration config;
+    private FileConfiguration messages;
+
+    public ConfigManager(AoiPlugin plugin) {
         this.plugin = plugin;
         load();
     }
 
     public void load() {
+
         plugin.saveDefaultConfig();
+        config = plugin.getConfig();
+
+        File msgFile = new File(plugin.getDataFolder(), "messages.yml");
+
+        if (!msgFile.exists()) {
+            plugin.saveResource("messages.yml", false);
+        }
+
+        messages = YamlConfiguration.loadConfiguration(msgFile);
+    }
+
+    public String getMessage(String path) {
+        String msg = messages.getString(path, "&cMissing message: " + path);
+        return color(msg);
+    }
+
+    public String getMessage(String path, String... placeholders) {
+
+        String msg = getMessage(path);
+
+        for (int i = 0; i < placeholders.length; i += 2) {
+            msg = msg.replace(placeholders[i], placeholders[i + 1]);
+        }
+
+        return msg;
+    }
+
+    private String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 
     public FileConfiguration getConfig() {
-        return plugin.getConfig();
+        return config;
     }
-
-    public int getAutoSaveInterval() {
-        return getConfig().getInt("auto-save-interval", 300);
-    }
-} 
+}
