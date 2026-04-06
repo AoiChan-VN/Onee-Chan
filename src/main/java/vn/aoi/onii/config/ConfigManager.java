3977d77c3725b8f934577e02085139c00e7b343a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-06 09:42:39 +0700

    Add files via upload

--- src/main/java/vn/aoi/onii/config/ConfigManager.java
+++ src/main/java/vn/aoi/onii/config/ConfigManager.java
@@ -1,1 +1,26 @@
+package vn.aoi.onii.config;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigManager {
+
+    private final JavaPlugin plugin;
+
+    public ConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+        load();
+    }
+
+    public void load() {
+        plugin.saveDefaultConfig();
+    }
+
+    public FileConfiguration getConfig() {
+        return plugin.getConfig();
+    }
+
+    public int getAutoSaveInterval() {
+        return getConfig().getInt("auto-save-interval", 300);
+    }
+} 
