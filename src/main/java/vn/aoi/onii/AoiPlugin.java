f316a325804e4eab1b0aecaa687fdcdba1c8d5ab
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-09 15:16:54 +0700

    Update AoiPlugin.java

--- src/main/java/vn/aoi/onii/AoiPlugin.java
+++ src/main/java/vn/aoi/onii/AoiPlugin.java
@@ -47,7 +47,7 @@ public class AoiPlugin extends JavaPlugin {
         // ================= MANAGER =================
 
         playerManager = new PlayerManager(repository);
-        realmManager = new RealmManager(ConfigManager);
+        realmManager = new RealmManager(configManager);
 
         cultivationService = new CultivationService(playerManager, realmManager, configManager);
 
