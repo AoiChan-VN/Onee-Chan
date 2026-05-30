713a1e45dfad571395d5e41bd8adc531c8905483
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-31 00:13:32 +0700

    Create SkillCategory.java

---
+++ src/main/java/vn/aoi/cultivation/model/skill/SkillCategory.java
@@ -0,0 +1,9 @@
+package vn.aoi.cultivation.model.skill;
+
+public enum SkillCategory {
+
+    CONG_KICH,   // offensive arts
+    PHONG_THU,   // defensive techniques
+    THAN_PHAP,   // movement / mobility
+    HO_TRO       // support / healing
+} 
